c67f8936f510fc1290901c395a6658b92df72993
JimBobJoe36 <larydbom@gmail.com>
2026-02-05 15:11:32 -0600

    Added the files

---
+++ Documentation/projectDetails.md
@@ -0,0 +1,22 @@
+# Option 2: ML Model for Real-World Problem (technical track)
+- Document process, evaluate accuracy, discuss limitations
+- Present use case, model performance, and ethical considerations
+- Deliverable: working code + demonstration
+
+# Planning and Reflection
+Problem: Abnormally high CPU/RAM usage
+
+Proposed Solution: Python program that gets the current CPU usage, and flags programs that takes up high amounts of data, either terminating or alerting the user of the process
+
+Failures: 
+- Detects Windows services, and flags them
+
+Reflection (Based on Flow Chart):
+- Was TFidVectorizer ever needed?
+    - No.
+- Does IsolationForest work with floats?
+    - Yes, but it had to be turned into a 2-D array with numpy.
+- What is an "ArrayLike" data type?
+    - An "ArrayLike" data type is a custom data type utilized by numpy, signalling that the data has to be an array. The array could be a string, float, integer, etc.
+- How simple would it be to implement finding the usual culprits?
+    - It would require a few tweaks to the code. The bigger issue is that the same processes that are flagged are consistently flagged at different states of running. In order to do this, the Windows processes would have to be exempted from that, as they cannot be "turned off".
---
+++ main.py
@@ -0,0 +1,118 @@
+# Import statements
+import os
+import subprocess
+import sys
+# Install packages
+packages = [
+    "scikit-learn",
+    "psutil",
+    "numpy"
+]
+
+def installPkg(package):
+    '''
+    Installs Python packages, based on the value passed into the function
+
+    :param package: the Python package to be installed
+    '''
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-qqq", package])
+
+def clear() -> None:
+    '''
+    Clears the terminal, allowing it to be easily read
+    '''
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+try:
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.ensemble import IsolationForest
+    import psutil
+    import numpy
+except Exception:
+    for pkg in packages:
+        installPkg(pkg)
+        clear()
+
+    # Import after installed
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.ensemble import IsolationForest
+    import psutil
+    import numpy
+
+# Global Variables
+processes = {}
+saveDirectoryPath = f"C:\\Users\\{os.getlogin()}\\Desktop"
+
+# Function definitions
+def collectData() -> dict:
+    '''
+    Collects the process name and the memory that the process is taking up
+    
+    :return processes: The process name and process memory, in Mb
+    :rtype processes: dict
+    '''
+    for proc in psutil.process_iter(['pid', 'name', 'memory_info']):
+        info = proc.info
+        processID = info["pid"]
+        processName = info['name']
+        # Memory usage (in Mb)
+        processMemory = info['memory_info'].rss / (1024 * 1024)
+
+        # Sets a key-value pair. Example would be "chrome.exe":0.234
+        processes[processName] = processMemory
+
+    return processes
+
+def searchForOutliers(data: dict, savePath: str) -> None:
+    '''
+    Docstring for searchForOutliers
+    
+    :param data: The data to search for outliers. For this purpose, we will pass in a dictionary with
+    the name of process and the total bytes used.
+    :type data: dict
+    :param savePath: The directory to save the output to
+    :type savePath: str
+    '''
+    # Setup
+    keys = []
+    values = []
+    for key in data: keys.append(key); values.append(data[key])
+    outlierDetector = IsolationForest(contamination=0.2, random_state=42)
+
+    # Get all outliers
+    valArray = numpy.array(values)
+    valArray = valArray.reshape(-1, 1)
+    outlierList = outlierDetector.fit_predict(valArray)
+
+    # Recombine Data
+    recombinedData = {}
+    for key, out in zip(keys, outlierList):
+        recombinedData[key] = int(out)  # 1 for inlier, -1 for outlier
+    
+    # Log Data, not necessary until the end
+    with open(f"{savePath}\\memData.txt", "w") as save:
+        for entry in data:
+            print(f"{entry}: {data[entry]}")
+            save.write(f"{entry}: {data[entry]} \n")
+    
+    # TODO Change $data to the dict of outliers
+    with open(f"{savePath}\\memOutlier.txt", "w") as outs:
+        for outlier in recombinedData:
+            if recombinedData[outlier] == -1:
+                outs.write(f'{outlier}\n')
+    
+    
+    # Load vectorizer (Maybe? I need to determine how to work with lists, in this case)
+    #vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(1,8))
+
+# main loop
+if __name__ == "__main__":
+    processes = collectData()
+    for entry in processes:
+            print(f"{entry}: {processes[entry]}") 
+    searchForOutliers(processes, saveDirectoryPath)
+    clear()
+    print("[END] Outliers have been found. Results stored on Desktop")
